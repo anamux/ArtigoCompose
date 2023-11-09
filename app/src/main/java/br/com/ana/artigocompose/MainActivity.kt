1d4a26e72d8b666a8e2a32722569f5c920f9324d
anamux <anaamux@gmail.com>
2023-11-09 13:51:06 -0300

    Adicionado código local

---
+++ app/src/main/java/br/com/ana/artigocompose/MainActivity.kt
@@ -0,0 +1,101 @@
+package br.com.ana.artigocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.structuralEqualityPolicy
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.ana.artigocompose.ui.theme.ArtigoComposeTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtigoComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(
+                        title = stringResource(R.string.article_title),
+                        message = stringResource(R.string.text_1),
+                        message2 = stringResource(R.string.text_2)
+
+                    )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun GreetingImage (){
+    val image = painterResource(R.drawable.bg_compose_background)
+    Box{
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+fun GreetingText(title: String,
+                 message: String,
+                 message2: String,
+                 modifier: Modifier = Modifier)
+{
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        GreetingImage()
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = message,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = message2,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun GreetingPreview() {
+    ArtigoComposeTheme {
+        GreetingText(
+            title = stringResource(R.string.article_title),
+            message = stringResource(R.string.text_1),
+            message2 = stringResource(R.string.text_2)
+        )
+    }
+}
